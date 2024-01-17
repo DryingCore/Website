1183aa4fc8d0f97d15a91515568bc37d38419377
Lein0rb4gX <lein0rb4g@gmail.com>
2024-01-17 20:53:25 -0300

    Changes to be committed: 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 document.getElementById("home").addEventListener("click", () => {
-	window.location.href = "./index.html";
+	window.location.href = "Website/index.html";
 });
 
 document.getElementById("graduation").addEventListener("click", () => {
-	window.location.href = "./certificados.html";
+	window.location.href = "Website/certificados.html";
 });
 
 console.log(window.location)
 
-if (window.location.pathname === "/certificados.html") {
+if (window.location.pathname === "Website/certificados.html") {
     alert("Você pode baixar os certificados clicando sobre a imagem")
 }
