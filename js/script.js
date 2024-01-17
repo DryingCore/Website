5b1b60d50033b7c2894fbe5906a30d8126671f31
Lein0rb4gX <lein0rb4g@gmail.com>
2024-01-17 20:38:56 -0300

    Changes to be committed: 	deleted:    css/index.css 	new file:   css/index/animation.css 	new file:   css/index/footer.css 	new file:   css/index/header.css 	new file:   css/index/index.css 	new file:   css/index/main.css 	new file:   css/index/mediascreen.css 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.getElementById("home").addEventListener("click", () => {
-	window.location.href = "../index.html";
+	window.location.href = "Website/index.html";
 });
 
 document.getElementById("graduation").addEventListener("click", () => {
